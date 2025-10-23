45905ddafd721bd549d56502b3c788d1ff76d123
William01br <williamstcorreia123@gmail.com>
2025-10-23 15:25:17 -0300

    feat: add movie entity

---
+++ src/modules/movies/entities/movie.entity.ts
@@ -0,0 +1,11 @@
+export class MovieEntity {
+  id: number;
+  url: string;
+  title: string;
+  genre: string;
+  ageGroup: number;
+  duration: string;
+  rating: number;
+  year: number;
+  description: string;
+}
